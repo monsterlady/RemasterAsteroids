346608a536482a1f753b055d374720ed868fb7cb
Bestitz <xuruikang6@hotmail.com>
2020-10-08 02:24:59 +0200

    - init

---
+++ src/App.java
@@ -0,0 +1,39 @@
+public class App {
+    /******************************************************************************
+     Asteroids, Version 1.3
+
+     Copyright 1998-2001 by Mike Hall.
+     Please see http://www.brainjar.com for terms of use.
+
+     Revision History:
+
+     1.01, 12/18/1999: Increased number of active photons allowed.
+     Improved explosions for more realism.
+     Added progress bar for loading of sound clips.
+     1.2,  12/23/1999: Increased frame rate for smoother animation.
+     Modified code to calculate game object speeds and timer
+     counters based on the frame rate so they will remain
+     constant.
+     Improved speed limit checking for ship.
+     Removed wrapping of photons around screen and set a fixed
+     firing rate.
+     Added sprites for ship's thrusters.
+     1.3,  01/25/2001: Updated to JDK 1.1.8.
+
+     Usage:
+
+     <applet code="Asteroids.class" width=w height=h></applet>
+
+     Keyboard Controls:
+
+     S            - Start Game    P           - Pause Game
+     Cursor Left  - Rotate Left   Cursor Up   - Fire Thrusters
+     Cursor Right - Rotate Right  Cursor Down - Fire Retro Thrusters
+     Spacebar     - Fire Cannon   H           - Hyperspace
+     M            - Toggle Sound  D           - Toggle Graphics Detail
+
+     ******************************************************************************/
+    public static void main(String[] args) {
+
+    }
+}
